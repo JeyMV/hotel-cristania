e321682fbd6bb3adb876fc0549b11ac1e4381913
Nzongo Pedro <nzongopedro3@gmail.com>
2024-02-06 16:57:54 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+require './vendor/autoload.php';
+
+define('URL', 'http://' . $_SERVER['SERVER_NAME'] . '/SGH');
+define('IMAGES', URL . '/storage/images/');
+define('CSS', URL . '/public/css/');
+define('JS', URL . '/public/js/');
+define('BOOTSTRAP_CSS', URL . '/plugins/bootstrap/css/bootstrap.min.css');
+define('BOOTSTRAP_JS_BUNDLE', URL . '/plugins/bootstrap/js/bootstrap.bundle.min.js');
+define('BOOTSTRAP_JS', URL . '/plugins/bootstrap/js/bootstrap.min.js');
